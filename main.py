8ee233ab14c82395f74f89577bd3c4480d9b1908
Michael Lockyer <mdlockyer@gmail.com>
2022-02-01 12:24:57 -0600

    Added entry point script at top level to fix import issues

---
+++ main.py
@@ -0,0 +1,12 @@
+from asitop.asitop import main
+
+
+if __name__ == '__main__':
+    powermetrics_process = main()
+    try:
+        powermetrics_process.terminate()
+        print("Successfully terminated powermetrics process")
+    except Exception as e:
+        print(e)
+        powermetrics_process.terminate()
+        print("Successfully terminated powermetrics process")
